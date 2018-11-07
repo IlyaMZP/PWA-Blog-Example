f7fa05e4c27e9c6b3a75021857d81a6c56b0acc3
Ilya <vasya.barabashkin@gmail.com>
2018-11-07 15:01:14 +0300

    Beautify

--- menu.js
+++ menu.js
@@ -1,3 +1,41 @@
-(function(){'use strict';var menuIconElement=document.querySelector('.header__icon');var menuElement=document.querySelector('.menu');var menuOverlayElement=document.querySelector('.menu__overlay');menuIconElement.addEventListener('click',showMenu,!1);menuOverlayElement.addEventListener('click',hideMenu,!1);menuElement.addEventListener('transitionend',onTransitionEnd,!1);function showMenu(){menuElement.style.transform="translateX(0)";menuElement.classList.add('menu--show');menuOverlayElement.classList.add('menu__overlay--show')}
-function hideMenu(){menuElement.style.transform="translateX(-110%)";menuElement.classList.remove('menu--show');menuOverlayElement.classList.remove('menu__overlay--show');menuElement.addEventListener('transitionend',onTransitionEnd,!1)}
-var touchStartPoint,touchMovePoint;document.body.addEventListener('touchstart',function(event){touchStartPoint=event.changedTouches[0].pageX;touchMovePoint=touchStartPoint},!1);document.body.addEventListener('touchmove',function(event){touchMovePoint=event.touches[0].pageX;if(touchStartPoint<10&&touchMovePoint>30){menuElement.style.transform="translateX(0)"}},!1);function onTransitionEnd(){if(touchStartPoint<10){menuElement.style.transform="translateX(0)";menuOverlayElement.classList.add('menu__overlay--show');menuElement.removeEventListener('transitionend',onTransitionEnd,!1)}}})()
+(function() {
+    'use strict';
+    var menuIconElement = document.querySelector('.header__icon');
+    var menuElement = document.querySelector('.menu');
+    var menuOverlayElement = document.querySelector('.menu__overlay');
+    menuIconElement.addEventListener('click', showMenu, !1);
+    menuOverlayElement.addEventListener('click', hideMenu, !1);
+    menuElement.addEventListener('transitionend', onTransitionEnd, !1);
+
+    function showMenu() {
+        menuElement.style.transform = "translateX(0)";
+        menuElement.classList.add('menu--show');
+        menuOverlayElement.classList.add('menu__overlay--show')
+    }
+
+    function hideMenu() {
+        menuElement.style.transform = "translateX(-110%)";
+        menuElement.classList.remove('menu--show');
+        menuOverlayElement.classList.remove('menu__overlay--show');
+        menuElement.addEventListener('transitionend', onTransitionEnd, !1)
+    }
+    var touchStartPoint, touchMovePoint;
+    document.body.addEventListener('touchstart', function(event) {
+        touchStartPoint = event.changedTouches[0].pageX;
+        touchMovePoint = touchStartPoint
+    }, !1);
+    document.body.addEventListener('touchmove', function(event) {
+        touchMovePoint = event.touches[0].pageX;
+        if (touchStartPoint < 10 && touchMovePoint > 30) {
+            menuElement.style.transform = "translateX(0)"
+        }
+    }, !1);
+
+    function onTransitionEnd() {
+        if (touchStartPoint < 10) {
+            menuElement.style.transform = "translateX(0)";
+            menuOverlayElement.classList.add('menu__overlay--show');
+            menuElement.removeEventListener('transitionend', onTransitionEnd, !1)
+        }
+    }
+})()
